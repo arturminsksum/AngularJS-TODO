c46c46c112f3d41baa88cfa85084828d6abbf23d
Artur Minsk <artur.minsk.sum@gmail.com>
2018-03-18 22:17:19 +0300

    Add AngularJS

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/app.module.js',
+  output: {
+    path: path.resolve('./dist/js/'),
+    filename: './bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.html'],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          mode: 'development',
+        },
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template.html',
+      filename: './index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
